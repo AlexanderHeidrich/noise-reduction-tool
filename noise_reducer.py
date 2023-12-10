4244caa28bbe6e1ef4d3f847a58944b9bd9ecc8d
alexander.heidrich <alexander.heidrich@basilicom.de>
2023-12-10 10:33:53 +0100

    Use only Noisegate

--- noise_reducer.py
+++ noise_reducer.py
@@ -25,9 +25,6 @@ def remove_noise(input_files):
 
         board = Pedalboard([
             NoiseGate(threshold_db=-30, ratio=1.5, release_ms=250),
-            Compressor(threshold_db=-16, ratio=2.5),
-            LowShelfFilter(cutoff_frequency_hz=400, gain_db=10, q=1),
-            Gain(gain_db=10)
         ])
 
         effected = board(reduced_noise, samplerate)
